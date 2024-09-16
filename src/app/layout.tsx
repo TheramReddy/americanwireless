59f6dcfeae9c6af39c03f3cb4c4a63da0d547616
THERAMREDDY DASAVANTH REDDY <71059812+TheramReddy@users.noreply.github.com>
2024-09-16 12:56:56 -0400

    Uploading to GITHUB

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+import {cn} from '@/lib/utils'
+import Navbar from './components/Navbar'
+
+
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+  variable: "--font-geist-sans", 
   weight: "100 900",
 });
 const geistMono = localFont({
@@ -24,11 +31,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en' className='h-full'>
+      
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={cn(
+          'relative h-full font-sans antialiased',
+          inter.className
+        )}>
+        <main className='relative flex flex-col min-h-screen'>
+          <Navbar />
+      
+            <div className='flex-grow flex-1'>
+              {children}
+            </div>
+        </main>
       </body>
     </html>
   );
